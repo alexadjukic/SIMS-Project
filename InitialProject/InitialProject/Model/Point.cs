cd5c6f03b839753cd0ab457c58165be5759d8e5d
Aleksa Djukic <aleksadjukic25@gmail.com>
2023-03-13 20:47:12 +0100

    fix: Make model classes public and inherit ISerializable

--- InitialProject/InitialProject/Model/Point.cs
+++ InitialProject/InitialProject/Model/Point.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InitialProject.Serializer;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace InitialProject.Model
 {
-    internal class Point
+    public class Point : ISerializable
     {
         public int Id { get; set; }
         public string Name { get; set; }
