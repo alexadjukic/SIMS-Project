a4bfc2d71b5e766112a521f88ac6566fc64b8b61
Leopoldina <leopoldinica123@gmail.com>
2023-03-16 12:01:17 +0100

    feat: Add Model and Repository for rating

---
+++ InitialProject/InitialProject/Repository/RatingRepository.cs
@@ -0,0 +1,51 @@
+﻿using InitialProject.Model;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Repository
+{
+    class RatingRepository
+    {
+        private const string FilePath = "../../../Resources/Data/ratings.csv";
+
+        private readonly Serializer<Rating> _serializer;
+
+        private List<Rating> _ratings;
+
+        public RatingRepository()
+        {
+            _serializer = new Serializer<Rating>();
+            _ratings = _serializer.FromCSV(FilePath);
+        }
+
+        public List<Rating> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Rating Save(Rating rating)
+        {
+            rating.Id = NextId();
+            _ratings = _serializer.FromCSV(FilePath);
+            _ratings.Add(rating);
+            _serializer.ToCSV(FilePath, _ratings);
+            return rating;
+        }
+
+        public int NextId()
+        {
+            _ratings = _serializer.FromCSV(FilePath);
+
+            if (_ratings.Count < 1)
+            {
+                return 1;
+            }
+
+            return _ratings.Max(t => t.Id) + 1;
+        }
+    }
+}
