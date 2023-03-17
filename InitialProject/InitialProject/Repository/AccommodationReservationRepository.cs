61faebb0c09262f2491d10fac4c880855382cbab
Luka Savkov <lukasavkov1@gmail.com>
2023-03-17 16:16:05 +0100

    feat: Add Model and Repository for accommodation reservation

---
+++ InitialProject/InitialProject/Repository/AccommodationReservationRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InitialProject.Model;
+using InitialProject.Serializer;
+
+namespace InitialProject.Repository
+{
+    public class AccommodationReservationRepository
+    {
+        private const string FilePath = "../../../Resources/Data/accommodationReservations.csv";
+
+        private readonly Serializer<AccommodationReservation> _serializer;
+
+        private List<AccommodationReservation> _accommodationReservations;
+
+        public AccommodationReservationRepository()
+        {
+            _serializer = new Serializer<AccommodationReservation>();
+            _accommodationReservations = _serializer.FromCSV(FilePath);
+        }
+
+        public List<AccommodationReservation> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public AccommodationReservation Save(DateTime startDate, DateTime endDate, int lenghtOfStay, int accommodationId, int guestId)
+        {
+            int id = NextId();
+
+            AccommodationReservation accommodationReservation = new AccommodationReservation(id, startDate, endDate, lenghtOfStay, accommodationId, guestId);
+
+            _accommodationReservations.Add(accommodationReservation);
+            _serializer.ToCSV(FilePath, _accommodationReservations);
+            return accommodationReservation;
+        }
+
+        public int NextId()
+        {
+            _accommodationReservations = _serializer.FromCSV(FilePath);
+            if (_accommodationReservations.Count < 1)
+            {
+                return 1;
+            }
+
+            return _accommodationReservations.Max(c => c.Id) + 1;
+        }
+    }
+}
