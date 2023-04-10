80e304b4c55fb287c47bb3332c98ac95bfdec729
Aleksa Djukic <aleksadjukic25@gmail.com>
2023-04-10 20:34:54 +0200

    feat: Add Injector

---
+++ InitialProject/InitialProject/Injector/Injector.cs
@@ -0,0 +1,29 @@
+﻿using InitialProject.Domain.RepositoryInterfaces;
+using InitialProject.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace InitialProject
+{
+    public class Injector
+    {
+        private static Dictionary<Type, object> _implementations = new Dictionary<Type, object>
+        {
+            { typeof(IUserRepository), new UserRepository() },
+            { typeof(ITourRepository), new TourRepository() },
+            { typeof(ILocationRepository), new LocationRepository() }
+        };
+
+        public static T CreateInstance<T>()
+        {
+            Type type = typeof(T);
+
+            if (_implementations.ContainsKey(type))
+            {
+                return (T)_implementations[type];
+            }
+
+            throw new ArgumentException($"No implementation found for type {type}");
+        }
+    }
+}
