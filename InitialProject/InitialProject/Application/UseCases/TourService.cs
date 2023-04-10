29cd6d0fe391a639d27df8c15632471d49e91abb
Aleksa Djukic <aleksadjukic25@gmail.com>
2023-04-10 20:34:25 +0200

    feat: Add TourService

---
+++ InitialProject/InitialProject/Application/UseCases/TourService.cs
@@ -0,0 +1,48 @@
+﻿using InitialProject.Domain.Models;
+using InitialProject.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InitialProject.Application.UseCases
+{
+    public class TourService
+    {
+        private readonly ITourRepository _tourRepository;
+        private readonly ILocationRepository _locationRepository;
+
+        public TourService()
+        {
+            _tourRepository = Injector.CreateInstance<ITourRepository>();
+            _locationRepository = Injector.CreateInstance<ILocationRepository>();
+        }
+
+        public IEnumerable<Tour> GetFutureTours()
+        {
+            var futureTours = _tourRepository.GetAll().Where(t => t.StartTime.Subtract(DateTime.Now).TotalHours > 0);
+            LoadLocations(futureTours);
+            return futureTours;
+        }
+
+        public IEnumerable<Tour> GetPastTours()
+        {
+            var pastTours = _tourRepository.GetAll().Where(t => t.StartTime.Subtract(DateTime.Now).TotalHours < 0);
+            LoadLocations(pastTours);
+            return pastTours;
+        }
+
+        public void CancelTour(Tour tour)
+        {
+            tour.Status = TourStatus.CANCELED;
+            _tourRepository.Update(tour);
+        }
+
+        private void LoadLocations(IEnumerable<Tour> tours)
+        {
+            foreach (var tour in tours)
+            {
+                tour.Location = _locationRepository.GetById(tour.LocationId);
+            }
+        }
+    }
+}
