cea1f0cbd413f263e85d73cd6f7de50ee16ffc36
Aleksa Djukic <aleksadjukic25@gmail.com>
2023-04-11 20:58:31 +0200

    feat: Add TourReview model and repository

---
+++ InitialProject/InitialProject/Domain/RepositoryInterfaces/ITourReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using InitialProject.Domain.Models;
+using System.Collections.Generic;
+
+namespace InitialProject.Domain.RepositoryInterfaces
+{
+    public interface ITourReviewRepository
+    {
+        public List<TourReview> GetAll();
+        public TourReview Save(TourReview review);
+        public int NextId();
+        public void Delete(TourReview review);
+        public TourReview Update(TourReview review);
+    }
+}
