76150e882e44c7116dedfa3986ebc9b0f0738bea
Leopoldina <leopoldinica123@gmail.com>
2023-04-15 16:46:01 +0200

    feat: Add requests overview windows

---
+++ InitialProject/InitialProject/WPF/ViewModels/OwnerViewModels/RequestDeclinedFormViewModel.cs
@@ -0,0 +1,28 @@
+﻿using InitialProject.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InitialProject.WPF.ViewModels.OwnerViewModels
+{
+    public class RequestDeclinedFormViewModel
+    {
+        private readonly Window _requestDeclinedForm;
+
+        public RequestDeclinedFormViewModel(Window requestDeclinedForm)
+        {
+            _requestDeclinedForm = requestDeclinedForm;
+
+            CloseWindowCommand = new RelayCommand(CloseWindowCommand_Execute);
+        }
+
+        public RelayCommand CloseWindowCommand { get; }
+        public void CloseWindowCommand_Execute(object? parameter)
+        {
+            _requestDeclinedForm.Close();
+        }
+    }
+}
---
+++ InitialProject/InitialProject/WPF/ViewModels/OwnerViewModels/RequestsOverviewViewModel.cs
@@ -0,0 +1,43 @@
+﻿using InitialProject.Commands;
+using InitialProject.WPF.Views.OwnerViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InitialProject.WPF.ViewModels.OwnerViewModels
+{
+    public class RequestsOverviewViewModel
+    {
+        private readonly Window _requestsOverview;
+
+        public RequestsOverviewViewModel(Window requestsOverview)
+        {
+            _requestsOverview = requestsOverview;
+
+            CloseWindowCommand = new RelayCommand(CloseWindowCommand_Execute);
+            DeclineRequestCommand = new RelayCommand(DeclineRequestCommand_Execute, DeclineRequestCommand_CanExecute);
+        }
+
+        public RelayCommand CloseWindowCommand { get; }
+        public RelayCommand DeclineRequestCommand { get; }
+
+        public void CloseWindowCommand_Execute(object? parameter)
+        {
+            _requestsOverview.Close();
+        }
+
+        public bool DeclineRequestCommand_CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void DeclineRequestCommand_Execute(object? parameter)
+        {
+            RequestDeclinedForm requestDeclinedForm = new RequestDeclinedForm();
+            requestDeclinedForm.Show();
+        }
+    }
+}
