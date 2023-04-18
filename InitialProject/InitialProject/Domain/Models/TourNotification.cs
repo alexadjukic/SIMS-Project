59b22df9c33e32c81cc17c279e44bc1265130860
Jovan Jokić <jovanjokic555@gmail.com>
2023-04-18 15:06:56 +0200

    refactor: Change some fields in TourNotification

--- InitialProject/InitialProject/Domain/Models/TourNotification.cs
+++ InitialProject/InitialProject/Domain/Models/TourNotification.cs
@@ -9,21 +9,26 @@ using System.Xml.Linq;
 
 namespace InitialProject.Domain.Models
 {
+    public enum NotificationStatus
+    {
+        READ = 1,
+        NOT_READ
+    }
     public class TourNotification : ISerializable
     {
         public int Id{ get; set; } 
         public string TextContent { get; set; }
-        public bool Seen { get; set; }
+        public NotificationStatus Status { get; set; }
         public DateTime ArrivalTime { get; set; }
         public int UserId { get; set; }
         public int GuideId { get; set; }
         public int TourId { get; set; }
         public Tour Tour { get; set; }
 
-        public TourNotification(string textContent, bool seen, DateTime arrivalTime, int userId, int guideId, int tourId)
+        public TourNotification(string textContent, NotificationStatus status, DateTime arrivalTime, int userId, int guideId, int tourId)
         {
             TextContent = textContent;
-            Seen = seen;
+            Status = status;
             ArrivalTime = arrivalTime;
             UserId = userId;
             GuideId = guideId;
@@ -36,7 +41,7 @@ namespace InitialProject.Domain.Models
 
         public string[] ToCSV()
         {
-            string[] csvValues = { Id.ToString(), TextContent, Seen.ToString(), ArrivalTime.ToString(), UserId.ToString(), GuideId.ToString(), TourId.ToString() };
+            string[] csvValues = { Id.ToString(), TextContent, Status.ToString(), ArrivalTime.ToString(), UserId.ToString(), GuideId.ToString(), TourId.ToString() };
             return csvValues;
         }
 
@@ -44,7 +49,7 @@ namespace InitialProject.Domain.Models
         {
             Id = int.Parse(values[0]);
             TextContent = values[1];
-            Seen = bool.Parse(values[2]);
+            Status = Enum.Parse<NotificationStatus>(values[2]);
             ArrivalTime = DateTime.Parse(values[3]);
             UserId = int.Parse(values[4]);
             GuideId = int.Parse(values[5]);
