096fe9a65f9af7ccad946f6bf626839e703cbd06
Aleksa Djukic <aleksadjukic25@gmail.com>
2023-04-19 08:05:32 +0200

    fix: Properly load tour reviews

--- InitialProject/InitialProject/Application/UseCases/TourReviewService.cs
+++ InitialProject/InitialProject/Application/UseCases/TourReviewService.cs
@@ -37,7 +37,7 @@ namespace InitialProject.Application.UseCases
             List<TourReview> reviews = new();
             foreach(var review in _tourReviewRepository.GetAll())
             {
-                var arrival = _checkpointArrivalRepository.GetById(review.Id);
+                var arrival = _checkpointArrivalRepository.GetById(review.ArrivalId);
                 var reservation = _tourReservationRepository.GetById(arrival.ReservationId);
                 reservation.User = _userRepository.GetById(reservation.UserId);
                 arrival.Checkpoint = _checkpointRepository.GetById(arrival.CheckpointId);
