03770ad2b5a4579f9ec60d67a344b5ea7014823d
Luka Savkov <lukasavkov1@gmail.com>
2023-06-10 13:20:00 +0200

    feat: Make page and viewmodel for Thread

---
+++ InitialProject/InitialProject/WPF/ViewModels/Guest1ViewModels/ThreadViewModel.cs
@@ -0,0 +1,43 @@
+﻿using InitialProject.Commands;
+using InitialProject.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.WPF.ViewModels.Guest1ViewModels
+{
+    public class ThreadViewModel : ViewModelBase
+    {
+        #region PROPERTIES
+        private Comment? _newComment;
+        public Comment? NewComment
+        {
+            get
+            {
+                return _newComment;
+            }
+            set
+            {
+                if (_newComment != value)
+                {
+                    _newComment = value;
+                    OnPropertyChanged(nameof(NewComment));
+                }
+            }
+        }
+        public Forum SelectedForum { get; set; }
+        public ObservableCollection<Comment> Comments { get; set; }
+        #endregion
+        public ThreadViewModel()
+        {
+
+        }
+
+        #region COMMANDS
+        public RelayCommand PostCommentCommand { get; }
+        #endregion
+    }
+}
