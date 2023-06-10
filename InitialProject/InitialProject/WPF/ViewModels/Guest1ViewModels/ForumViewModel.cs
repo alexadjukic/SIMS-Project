4d99154401df4e9137e9740b0ccdb446cdd6e90d
Luka Savkov <lukasavkov1@gmail.com>
2023-06-10 12:56:38 +0200

    feat: Make page and viewmodel for Forum

---
+++ InitialProject/InitialProject/WPF/ViewModels/Guest1ViewModels/ForumViewModel.cs
@@ -0,0 +1,48 @@
+﻿using InitialProject.Commands;
+using InitialProject.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.WPF.ViewModels.Guest1ViewModels
+{
+    public class ForumViewModel : ViewModelBase
+    {
+        #region PROPERTIES
+        private Forum? _selectedForum;
+        public Forum? SelectedForum
+        {
+            get
+            {
+                return _selectedForum;
+            }
+            set
+            {
+                if (_selectedForum != value)
+                {
+                    _selectedForum = value;
+                    OnPropertyChanged(nameof(SelectedForum));
+                }
+            }
+        }
+        public ObservableCollection<Forum> Forums { get; set; }
+        #endregion
+        public ForumViewModel()
+        {
+
+        }
+
+        #region COMMANDS
+        public RelayCommand ViewForumCommand { get; }
+        public RelayCommand NewThreadCommand { get; }
+
+        public void ViewForumCommand_Execute(object? parameter)
+        {
+
+        }
+        #endregion
+    }
+}
